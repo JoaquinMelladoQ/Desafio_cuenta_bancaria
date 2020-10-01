91aa2ecd2cdaa9a533dda6c660c57e70ccad9a32
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-30 21:11:40 -0300

    times was created and tested with values to push to an empty array

--- carta.rb
+++ carta.rb
@@ -1,12 +1,20 @@
 class Carta
-    attr_accessor :pinta, :numero
+    attr_accessor :pinta, 
+    :numero
     def initialize(pinta, numero)
         @pinta = pinta.sample
         @numero = numero
     end
 end
 
-carta1 = Carta.new(["c", "d", "e", "t"], Random.rand(1...13))
+# carta1 = Carta.new(["c", "d", "e", "t"], Random.rand(1...13))
 
-puts carta1.pinta
+# puts carta1.pinta
 
+deck = []
+5.times do
+    deck.push(Carta.new(["c", "d", "e", "t"], Random.rand(1...13)))
+end
+
+puts deck[0].pinta
+puts deck[1].numero
